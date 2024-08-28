3056fc4713948efe8ed8bb0884a958f69f17337a
RearoseSese <157780959+RearoseSese@users.noreply.github.com>
2024-08-28 10:40:11 -1200

    s

--- HandsOn3/components/PokemonCard.js
+++ HandsOn3/components/PokemonCard.js
@@ -14,42 +14,43 @@ const getTypeDetails = (type) => {
             return {borderColor: "#A0A0A0", emoji: "❓"};
     }
 }
-export default function Pokemon_Cards ({
+export default function pokemonCard ({
     name,
     image,
     type,
     hp,
     moves,
     weaknesses
-}) {
-    const { borderColor, emoji } = getTypeDetails(type);
-    return (
-        <View style={styles.card}>
-            <View style={styles.nameContainer}>
-                <Text style={styles.name}>{name}</Text>
-                <Text style={styles.hp}>❤️{hp}</Text>
-            </View>
-
-            <Image source={image} style={styles.image} accesibilityLabel={`${name} pokemon`} />
-
-            <View style ={styles.typeContainer}>
-                <View style={[styles.badge, { borderColor }]}>
-                    <Text style={styles.typeEmoji}>{emoji}</Text>
-                    <Text style={styles.typeText}>{type}</Text>
+     }) {
+   
+        const { borderColor, emoji } = getTypeDetails(type);
+        return (
+            <View style={styles.card}>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.name}>{name}</Text>
+                    <Text style={styles.hp}>❤️{hp}</Text>
+                </View>
+    
+                <Image source={image} style={styles.image} accesibilityLabel={`${name} pokemon`} />
+    
+                <View style ={styles.typeContainer}>
+                    <View style={[styles.badge, { borderColor }]}>
+                        <Text style={styles.typeEmoji}>{emoji}</Text>
+                        <Text style={styles.typeText}>{type}</Text>
+                    </View>
+                </View>
+    
+                <View style={styles.movesContainer}>
+                    <Text style={styles.movesText}>Moves:</Text>
+                    <Text style={styles.movesText}>{moves.join(", ")}</Text>
+                </View>
+    
+                <View style={styles.weaknessesContainer}>
+                    <Text style={styles.weaknessesText}>Weaknesses:</Text>
+                    <Text style={styles.weaknessesText}>{weaknesses.join(",")}</Text>
                 </View>
             </View>
-
-            <View style={styles.movesContainer}>
-                <Text style={styles.movesText}>Moves:</Text>
-                <Text style={styles.movesText}>{moves.join(", ")}</Text>
-            </View>
-
-            <View style={styles.weaknessesContainer}>
-                <Text style={styles.weaknessesText}>Weaknesses:</Text>
-                <Text style={styles.weaknessesText}>{weaknesses.join(", ")}</Text>
-            </View>
-        </View>
-    );
+        )
 }
 
 const styles = StyleSheet.create({
